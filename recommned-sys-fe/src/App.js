e63bbac340dd9e4af4f5d0d516bc13ec2f746ab7
CongPhiNguyen <congphinguyen312@gmail.com>
2023-02-06 01:45:03 +0700

    fix layout

--- recommned-sys-fe/src/App.js
+++ recommned-sys-fe/src/App.js
@@ -1,14 +1,72 @@
-import logo from "./logo.svg"
-import "./App.css"
-import { Button } from "antd"
+import React, { useState } from "react"
 import "antd/dist/antd.min.css"
-function App() {
+import "./index.css"
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined
+} from "@ant-design/icons"
+import { Layout, Menu } from "antd"
+
+const { Header, Sider, Content } = Layout
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false)
   return (
-    <div>
-      <Button type="primary">Nguyễn Công Phi</Button>
-      <p className="text-[red]">Phi</p>
-    </div>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="text-[white] p-[20px]">Recommend System</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1"
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2"
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3"
+            }
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0
+          }}
+        >
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed)
+            }
+          )}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
-
 export default App
